3c244439921c00c7812bc6ab9b7ce87306af48a7
nooh <nooh.saeed@gmail.com>
2024-09-15 01:52:28 +0300

    update

--- lib/core/models/single_todo.dart
+++ lib/core/models/single_todo.dart
@@ -1,37 +1,37 @@
-// To parse this JSON data, do
-//
-//     final singletodos = singletodosFromJson(jsonString);
+// // To parse this JSON data, do
+// //
+// //     final singletodos = singletodosFromJson(jsonString);
 
-import 'dart:convert';
+// import 'dart:convert';
 
-Singletodos singletodosFromJson(String str) => Singletodos.fromJson(json.decode(str));
+// Singletodos singletodosFromJson(String str) => Singletodos.fromJson(json.decode(str));
 
-String singletodosToJson(Singletodos data) => json.encode(data.toJson());
+// String singletodosToJson(Singletodos data) => json.encode(data.toJson());
 
-class Singletodos {
-    int id;
-    String todo;
-    bool completed;
-    int userId;
+// class Singletodos {
+//     int id;
+//     String todo;
+//     bool completed;
+//     int userId;
 
-    Singletodos({
-        required this.id,
-        required this.todo,
-        required this.completed,
-        required this.userId,
-    });
+//     Singletodos({
+//         required this.id,
+//         required this.todo,
+//         required this.completed,
+//         required this.userId,
+//     });
 
-    factory Singletodos.fromJson(Map<String, dynamic> json) => Singletodos(
-        id: json["id"],
-        todo: json["todo"],
-        completed: json["completed"],
-        userId: json["userId"],
-    );
+//     factory Singletodos.fromJson(Map<String, dynamic> json) => Singletodos(
+//         id: json["id"],
+//         todo: json["todo"],
+//         completed: json["completed"],
+//         userId: json["userId"],
+//     );
 
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "todo": todo,
-        "completed": completed,
-        "userId": userId,
-    };
-}
+//     Map<String, dynamic> toJson() => {
+//         "id": id,
+//         "todo": todo,
+//         "completed": completed,
+//         "userId": userId,
+//     };
+// }
